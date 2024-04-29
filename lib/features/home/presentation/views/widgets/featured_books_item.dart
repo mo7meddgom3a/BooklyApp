884fae4aa42a9f11734029204ca1f1f19d2d86f3
mo7meddgom3a <mohamedgomaa0115@gmail.com>
2024-04-29 15:16:40 +0300

    add FeaturedListViewItems

---
+++ lib/features/home/presentation/views/widgets/featured_books_item.dart
@@ -0,0 +1,29 @@
+
+import 'package:booklyapp/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedBooksItems extends StatelessWidget {
+  const FeaturedBooksItems({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2.7/4,
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        height: MediaQuery.of(context).size.height * 0.2,
+        width: 100,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          image: const DecorationImage(
+            image: AssetImage(
+              AssetsData.testImage,
+            ),
+            fit: BoxFit.fill,
+          ),
+          borderRadius: BorderRadius.circular(16),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,5 @@
 import 'package:booklyapp/features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:booklyapp/features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:booklyapp/features/home/presentation/views/widgets/featured_books_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,8 +12,25 @@ class HomeViewBody extends StatelessWidget {
       child: Column(
         children: [
           CustomAppBar(),
-          CustomListViewItems(),
+          FeaturedBoosListView(),
         ],
+      ),
+    );
+  }
+}
+class FeaturedBoosListView extends StatelessWidget {
+  const FeaturedBoosListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return const FeaturedBooksItems();
+        },
       ),
     );
   }
