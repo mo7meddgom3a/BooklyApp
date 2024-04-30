f322aafe1289f825b59df7b9b6ff113421ffc294
mo7meddgom3a <mohamedgomaa0115@gmail.com>
2024-04-30 15:07:59 +0300

    fix scrolling problem by adding customscrolview

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -15,69 +15,75 @@ class BookDetailsViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
 
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const CustomBookDetailsAppBar(),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: width * 0.24),
-              child: const CustomBookImage(),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "The Jungle Book",
-              style: Styles.textStyle30,
-            ),
-            const SizedBox(
-              height: 3,
-            ),
-            const Opacity(
-              opacity: 0.7,
-              child: Text(
-                "Rudyard Kipling",
-                style: Styles.textStyle18,
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+            hasScrollBody: false,
+            child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CustomBookDetailsAppBar(),
+              const SizedBox(
+                height: 20,
               ),
-            ),
-            const SizedBox(
-              height: 7,
-            ),
-            const BookRating(),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: width * 0.04),
-              child: const BooksActionButton(),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "You can also like",
-                style: Styles.textStyle16,
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * 0.24),
+                child: const CustomBookImage(),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const SimilarBooksListView(),
-            const SizedBox(
-              height: 30,
-            ),
-          ],
-        ),
-      ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "The Jungle Book",
+                style: Styles.textStyle30,
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              const Opacity(
+                opacity: 0.7,
+                child: Text(
+                  "Rudyard Kipling",
+                  style: Styles.textStyle18,
+                ),
+              ),
+              const SizedBox(
+                height: 7,
+              ),
+              const BookRating(),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * 0.04),
+                child: const BooksActionButton(),
+              ),
+              const Expanded(
+                child: SizedBox(
+                  height: 50,
+                ),
+              ),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "You can also like",
+                  style: Styles.textStyle16,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const SimilarBooksListView(),
+              const SizedBox(
+                height: 30,
+              ),
+              const SimilarBooksListView(),
+            ],
+          ),
+        ))
+      ],
     );
   }
 }
-
