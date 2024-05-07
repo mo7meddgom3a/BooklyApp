61f472b6427fcd984c2b8ede58dc5e10187b3356
mo7meddgom3a <mohamedgomaa0115@gmail.com>
2024-05-07 16:14:34 +0300

    create featured books cubit

--- lib/features/home/presentation/view_model/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/view_model/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,21 @@
-import 'package:bloc/bloc.dart';
 import 'package:booklyapp/features/home/data/models/book_model/book_model.dart';
-import 'package:meta/meta.dart';
-
+import 'package:booklyapp/features/home/data/repos/home_repo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this._homeRepo) : super(FeaturedBooksInitial());
+
+  final HomeRepo _homeRepo;
+
+  Future <void> fetchFeaturedBooks() async {
+    emit( FeaturedBooksLoading());
+    final result = await _homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) => emit(FeaturedBooksError(failure.errorMessage)),
+      (books) => emit(FeaturedBooksLoaded(books))
+    );
+
+  }
 }
