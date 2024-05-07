624e24eaf154c7795f97bcc5cf4ddd571750b481
mo7meddgom3a <mohamedgomaa0115@gmail.com>
2024-05-07 13:43:53 +0300

    create book model

--- lib/core/widgets/custom_book_image.dart
+++ lib/core/widgets/custom_book_image.dart
@@ -1,26 +1,34 @@
 
+import 'package:booklyapp/core/utils/app_router.dart';
 import 'package:booklyapp/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
-        height: MediaQuery.of(context).size.height * 0.2,
-        width: 100,
-        decoration: BoxDecoration(
-          color: Colors.red,
-          image: const DecorationImage(
-            image: AssetImage(
-              AssetsData.testImage,
+    return InkWell(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.KBookDetailsView);
+      },
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.2,
+          width: 100,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            image: const DecorationImage(
+              image: AssetImage(
+                AssetsData.testImage,
+              ),
+              fit: BoxFit.fill,
             ),
-            fit: BoxFit.fill,
+            borderRadius: BorderRadius.circular(16),
           ),
-          borderRadius: BorderRadius.circular(16),
         ),
       ),
     );
